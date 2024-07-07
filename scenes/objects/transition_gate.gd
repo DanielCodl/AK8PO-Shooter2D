e5b86ed6e58abac7cf6ca6f6ebea1ee6c8e45f03
DCODL <dd@dd.com>
2024-07-07 13:45:21 +0200

    Implementing fading out/in when moving to next level

--- scenes/objects/transition_gate.gd
+++ scenes/objects/transition_gate.gd
@@ -11,4 +11,4 @@ func _ready():
 		levels["underground"] = "res://scenes/levels/underground.tscn"
 
 func _on_body_entered(_body):
-	get_tree().change_scene_to_file(levels[level])
+	TransitionLayer.change_scene(levels[level])
