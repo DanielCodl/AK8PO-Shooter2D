f835de460bacbacdb7b5b37b04828aded91f7a0c
DCODL <dd@dd.com>
2024-07-09 08:48:27 +0200

    Updating shotgun damage in Bullet scene

--- global/global.gd
+++ global/global.gd
@@ -11,5 +11,5 @@ const enemy_parameters = {
 const gun_data = {
 	guns.AK: {'damage': 20, 'speed': 200, 'texture': preload("res://graphics/guns/projectiles/default.png")},
 	guns.ROCKET: {'damage': 60, 'speed': 120, 'texture': preload("res://graphics/guns/projectiles/large.png")},
-	guns.SHOTGUN: {'damage': 30, 'range': 40},
+	guns.SHOTGUN: {'damage': 30, 'range': 100},
 }
--- scenes/projectiles/bullet.gd
+++ scenes/projectiles/bullet.gd
@@ -16,8 +16,14 @@ func setup(pos, dir, type):
 		explosive = type == Global.guns.ROCKET
 	else:
 		$CollisionShape2D.disabled =  true
-		$Sprite2D.hide()
+		$Sprite2D.hide()		
 		$PointLight2D.hide()
+		for enemy in get_tree().get_nodes_in_group("Enemies"):
+			var bullet_angle = rad_to_deg(dir.angle())
+			var enemy_angle = rad_to_deg((enemy.position - pos).angle())
+			# enemy is close enough to the player
+			if pos.distance_to(enemy.position) < Global.gun_data[Global.guns.SHOTGUN]["range"] and abs(bullet_angle - enemy_angle) < 90:
+				enemy.hit(Global.gun_data[Global.guns.SHOTGUN]["damage"], enemy.get_sprites())
 
 func _process(delta):
 	position += direction * speed * delta
